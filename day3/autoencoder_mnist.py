5afd34070228cda4c69e8da4682312885d7e915c
Sangkyun Lee <sanglee77@gmail.com>
2017-08-25 02:27:32 +0900

    day3

---
+++ day3/autoencoder_mnist.py
@@ -0,0 +1,113 @@
+""" 
+Auto Encoder on MNIST handwritten digits.
+"""
+
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL']='2'
+
+import tensorflow as tf
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Import MNIST data
+from tensorflow.examples.tutorials.mnist import input_data
+mnist = input_data.read_data_sets("MNIST_data", one_hot=True)
+
+# Parameters
+learning_rate = 0.01
+training_epochs = 20
+batch_size = 256
+display_step = 1
+examples_to_show = 10
+
+# Network Parameters
+n_hidden_1 = 256 # 1st layer num features
+n_hidden_2 = 128 # 2nd layer num features
+n_input = 784 # MNIST data input (img shape: 28*28)
+
+# tf Graph input (only pictures)
+X = tf.placeholder(tf.float32, [None, n_input])
+
+weights = {
+    'encoder_h1': tf.Variable(tf.random_normal([?, ?])),
+    'encoder_h2': tf.Variable(tf.random_normal([?, ?])),
+    'decoder_h1': tf.Variable(tf.random_normal([?, ?])),
+    'decoder_h2': tf.Variable(tf.random_normal([?, ?])),
+}
+biases = {
+    'encoder_b1': tf.Variable(tf.random_normal([?])),
+    'encoder_b2': tf.Variable(tf.random_normal([?])),
+    'decoder_b1': tf.Variable(tf.random_normal([?])),
+    'decoder_b2': tf.Variable(tf.random_normal([?])),
+}
+
+
+# Building the encoder
+def encoder(x):
+    # Encoder Hidden layer with sigmoid activation #1
+    layer_1 = tf.nn.sigmoid(tf.add(tf.matmul(x, ?), ?))
+    # Encoder Hidden layer with sigmoid activation #2
+    layer_2 = tf.nn.sigmoid(tf.add(tf.matmul(?, ?), ?))
+
+    return layer_2
+
+# Building the decoder
+def decoder(x):
+    # Decoder Hidden layer with sigmoid activation #1
+    ?
+    # Decoder Hidden layer with sigmoid activation #2
+    ?
+
+    return layer_2
+
+
+
+# Construct model
+encoder_op = encoder(X)
+decoder_op = ?
+
+# Prediction
+y_pred = decoder_op
+# Targets (Labels) are the input data.
+y_true = X
+
+# Define loss and optimizer, minimize the squared error
+cost = tf.reduce_mean(tf.square(y_true - y_pred))
+optimizer = tf.train.RMSPropOptimizer(learning_rate).minimize(cost)
+
+# Initializing the variables
+init = tf.global_variables_initializer()
+
+# Launch the graph
+with tf.Session() as sess:
+    sess.run(init)
+
+    total_batch = int(mnist.train.num_examples/batch_size)
+
+    # Training cycle
+    for epoch in range(training_epochs):
+        # Loop over all batches
+        for i in range(total_batch):
+            batch_xs, batch_ys = mnist.train.next_batch(batch_size)
+            # Run optimization op (backprop) and cost op (to get loss value)
+            _, c = sess.run([optimizer, cost], feed_dict={X: ?})
+
+        # Display logs per epoch step
+        if epoch % display_step == 0:
+            print("Epoch:", '%04d' % (epoch+1),
+                  "cost=", "{:.9f}".format(c))
+
+    print("Optimization Finished!")
+
+    # Applying encode and decode over test set
+    encode_decode = sess.run(
+        ?, feed_dict={X: mnist.test.images[:examples_to_show]})
+
+    # Compare original images with their reconstructions
+    f, a = plt.subplots(2, 10, figsize=(10, 2))
+    for i in range(examples_to_show):
+        a[0][i].imshow(np.reshape(mnist.test.images[i], (28, 28)), cmap="gray")
+        a[1][i].imshow(np.reshape(encode_decode[i], (28, 28)), cmap="gray")
+    f.show()
+    plt.draw() 
+    plt.waitforbuttonpress()
